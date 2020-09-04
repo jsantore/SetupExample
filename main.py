ada3e536b048e3b7f9ab1428500a210e5678895a
johns <jsantore@bridgew.edu>
2020-09-04 16:11:08 -0400

    final commit for this project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 print("Hi Comp151")
-print("Welcome to the second week")
+print("Welcome to a crazy semester")
 numberStorage = 10
+print(numberStorage)
 numberStorage = numberStorage + 40
 print(numberStorage)
+print("and that is all for this video")
